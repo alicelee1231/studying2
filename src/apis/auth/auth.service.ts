fd9af41e0b2a4fc0a98e04c0510dab55922cb4fe
alicelee1231 <saemmilee1231@gmail.com>
2022-09-23 18:10:13 +0900

    test

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly jwtService: JwtService,
-
+    //d
     @Inject(CACHE_MANAGER)
     private readonly cacheManager: Cache,
   ) {}
